4f9f33e5aca2e4eb397822d1f59b48bbc1a147ef
Mohammed <dev.magha@gmail.com>
2019-09-12 10:21:10 +0300

    API: Pagination

--- API/app/Http/Controllers/CategoryController.php
+++ API/app/Http/Controllers/CategoryController.php
@@ -9,9 +9,14 @@ use Illuminate\Support\Facades\Response;
 
 class CategoryController extends Controller
 {
-    public function index () {
+    public function index (Request $request) {
+    	$last_index = $request->query('last_index');
+    	$limit = $request->query('limit');
+
+    	$offset = $last_index + 1;
+
     	$category_icon_path = config('constants.category_icon');
-    	$categories = Category::select('id', 'name', DB::raw("CONCAT('".$category_icon_path."', '/', icon) as icon"))->orderBy('created_at', 'DESC')->get();
+    	$categories = Category::select('id', 'name', DB::raw("CONCAT('".$category_icon_path."', '/', icon) as icon"))->orderBy('created_at', 'DESC')->limit($limit)->offset($offset)->get();
 
         $response = (new UtilityController)->json_select(["categories" => $categories]);
 
